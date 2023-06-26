605797bfc4e22285b0ce11f253ccaa79ec9037d5
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-06-26 20:48:17 +0530

    Move styled-components outside component cuz spam in console

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -3,6 +3,48 @@ import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 
+const Wrapper = styled.div`
+  margin: 4rem 0;
+`;
+const Card = styled.div`
+  min-height: 15rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 1rem;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    object-fit: cover;
+  }
+
+  p {
+    position: absolute;
+    z-index: 999;
+    left: 50%;
+    bottom: 0;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 40%;
+    // font-size: 1.5rem;
+    // font-weight: 700;
+  }
+`;
+const Gradient = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.6));
+`;
+
 const Popular = () => {
   const [popularRecipes, setPopularRecipes] = useState([]);
 
@@ -28,48 +70,6 @@ const Popular = () => {
     drag: "free",
     gap: "2rem",
   };
-  const Wrapper = styled.div`
-    margin: 4rem 0;
-  `;
-  const Card = styled.div`
-    min-height: 15rem;
-    border-radius: 2rem;
-    overflow: hidden;
-    position: relative;
-
-    img {
-      border-radius: 1rem;
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      left: 0;
-      object-fit: cover;
-    }
-
-    p {
-      position: absolute;
-      z-index: 999;
-      left: 50%;
-      bottom: 0;
-      transform: translate(-50% ,0%);
-      color: white;
-      width: 100%;
-      text-align: center;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 40%;
-      // font-size: 1.5rem;
-      // font-weight: 700;
-    }
-  `;
-
-  const Gradient = styled.div`
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.6));
-  `
 
   return (
     <div>
