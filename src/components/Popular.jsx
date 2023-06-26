594d95c1196d3f67be44e666e7c4d0872d21e377
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-06-26 19:28:14 +0530

    API request and setting up state

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,35 @@
-const Popular = () => {
-  return (
-    <div>Popular</div>
-  )
-}
+import { useEffect, useState } from "react";
 
-export default Popular
+const Popular = () => {
+  const [popularRecipes, setPopularRecipes] = useState([]);
+
+  const API_URL = `https://api.spoonacular.com/recipes/random?apiKey=${
+    import.meta.env.VITE_API_KEY
+  }&number=5`;
+
+  const fetchPopularRecipes = async () => {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    setPopularRecipes(data.recipes);
+  };
+
+  useEffect(() => {
+    fetchPopularRecipes();
+  }, []);
+
+  return (
+    <div>
+      <h1>Popular recipies</h1>
+      <p>Popular</p>
+      {popularRecipes.map((recipie)=>{
+        return(
+          <div key={recipie.id}>
+            {recipie.title}
+          </div>
+        )
+      })}
+    </div>
+  );
+};
+
+export default Popular;
