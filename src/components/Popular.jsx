a7dcf9fe5862921eba8e3fa1eb1bf91bc2a923c1
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-06-27 13:02:14 +0530

    Set items in localStorage

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,49 +1,8 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { Wrapper, Card, Gradient } from "../styles/myStyles";
 import "@splidejs/react-splide/css";
 
-const Wrapper = styled.div`
-  margin: 4rem 0;
-`;
-const Card = styled.div`
-  min-height: 15rem;
-  border-radius: 2rem;
-  overflow: hidden;
-  position: relative;
-
-  img {
-    border-radius: 1rem;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    left: 0;
-    object-fit: cover;
-  }
-
-  p {
-    position: absolute;
-    z-index: 999;
-    left: 50%;
-    bottom: 0;
-    transform: translate(-50%, 0%);
-    color: white;
-    width: 100%;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 40%;
-    // font-size: 1.5rem;
-    // font-weight: 700;
-  }
-`;
-const Gradient = styled.div`
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.6));
-`;
 
 const Popular = () => {
   const [popularRecipes, setPopularRecipes] = useState([]);
@@ -53,10 +12,17 @@ const Popular = () => {
   }&number=5`;
 
   const fetchPopularRecipes = async () => {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    console.log(data.recipes);
-    setPopularRecipes(data.recipes);
+    const checkLocalStorage = localStorage.getItem("popular");
+
+    if (checkLocalStorage) {
+      setPopularRecipes(JSON.parse(checkLocalStorage));
+    } else {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      // console.log(data.recipes);
+      setPopularRecipes(data.recipes);
+    }
   };
 
   useEffect(() => {
