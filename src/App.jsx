761c270619c8c906699e012b6b081fdde1f9cef9
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-06-27 22:46:58 +0530

    Add Logo

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import "./App.css";
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import Search from "./components/Search";
+import { Logo, Nav } from "./styles/myStyles";
+import { GiFoodTruck } from "react-icons/gi";
 
 function App() {
   return (
     <>
+      <Nav>
+        <Logo to="/">
+          <GiFoodTruck />
+          Foodie
+        </Logo>
+      </Nav>
       <Search />
       <Category />
       <Pages />
